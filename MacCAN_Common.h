f34ef3cee929a5ee41192af1b451baef069ff96b
makemake <makemake@067995c0-e9c5-481c-8c07-91041b31ea30>
2023-07-07 06:23:40 +0000

    Replaced error code `pipe stalled` (-51) by CAN API compatible error code `transmitter busy` (-20)
    Relates to [MACCAN-48]
    
    git-svn-id: https://uv-software.net/MacCAN/Core/trunk/src@1756 067995c0-e9c5-481c-8c07-91041b31ea30

--- MacCAN_Common.h
+++ MacCAN_Common.h
@@ -67,8 +67,8 @@
 #define CANUSB_ERROR_BUSY      (-20)
 #define CANUSB_ERROR_OK          (0)
 /* MacCAN specific error codes */
-#define CANUSB_ERROR_STALLED   (-51)
-#define CANUSB_SUCCESS  CANUSB_ERROR_OK
+#define CANUSB_ERROR_STALLED   CANUSB_ERROR_BUSY
+#define CANUSB_SUCCESS         CANUSB_ERROR_OK
 
 /* CAN API V3 compatible board states */
 #define CANUSB_BOARD_NOT_AVAILABLE  (-1)
